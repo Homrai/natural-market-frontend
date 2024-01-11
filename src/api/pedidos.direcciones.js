7ce7b3bc5d8cb4cfe8c16d63c363334ebfde345e
Homrai <90481686+Homrai@users.noreply.github.com>
2024-01-11 05:29:27 +0100

    Update pedidos.direcciones.js

--- src/api/pedidos.direcciones.js
+++ src/api/pedidos.direcciones.js
@@ -1,4 +1,4 @@
-const dominio = "https://natural-market.onrender.com/pedido/";
+const dominio = "https://natural-market-homrai.vercel.app/pedido/";
 export const pedidos= async (token, metodo, dir, registroDatos)=>{
     try {
         const datos = 
